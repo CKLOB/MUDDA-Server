b4ec56ccda93c022f064c5dc1ca9f912c644bfaf
박하민 <hamin090403@gmail.com>
2026-07-06 13:58:20 +0900

    chore: #5 :: add initial database migration

--- src/test/kotlin/team/cklob/mudda/MuddaApplicationTests.kt
+++ src/test/kotlin/team/cklob/mudda/MuddaApplicationTests.kt
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Test
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection
 import org.springframework.boot.test.context.SpringBootTest
 import org.testcontainers.containers.PostgreSQLContainer
+import org.testcontainers.utility.DockerImageName
 import org.testcontainers.junit.jupiter.Container
 import org.testcontainers.junit.jupiter.Testcontainers
 
@@ -22,9 +23,13 @@ class MuddaApplicationTests {
 	}
 
 	companion object {
+		private val postgisImage = DockerImageName
+			.parse("postgis/postgis:16-3.5-alpine")
+			.asCompatibleSubstituteFor("postgres")
+
 		@Container
 		@ServiceConnection
 		@JvmStatic
-		val postgres = PostgreSQLContainer<Nothing>("postgres:16-alpine")
+		val postgres = PostgreSQLContainer<Nothing>(postgisImage)
 	}
 }
