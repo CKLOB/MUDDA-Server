10be99cc67bbfff2ef051bd84a38fba10fc40bf9
hej090224 <fc49854985@gmail.com>
2026-07-27 19:58:39 +0900

    feat: #14 :: implement apple oauth authorization code exchange

--- src/main/kotlin/team/cklob/mudda/domain/auth/infrastructure/OAuthProperties.kt
+++ src/main/kotlin/team/cklob/mudda/domain/auth/infrastructure/OAuthProperties.kt
@@ -6,6 +6,7 @@ import org.springframework.boot.context.properties.ConfigurationProperties
 data class OAuthProperties(
     val kakao: Kakao,
     val google: Google,
+    val apple: Apple,
 ) {
     data class Kakao(
         val clientId: String,
@@ -19,5 +20,13 @@ data class OAuthProperties(
         val clientSecret: String = "",
         val tokenUri: String = "https://oauth2.googleapis.com/token",
         val userInfoUri: String = "https://openidconnect.googleapis.com/v1/userinfo",
+    )
+
+    data class Apple(
+        val clientId: String,
+        val teamId: String,
+        val keyId: String,
+        val privateKey: String,
+        val tokenUri: String = "https://appleid.apple.com/auth/token",
     )
 }
