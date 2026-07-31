43fc3b399df54aac362f65c42c92a70b6ab0e5f3
hej090224 <fc49854985@gmail.com>
2026-07-31 22:43:54 +0900

    refactor: #17 :: make ProfileAccessPolicy's visibility check an exhaustive when

--- src/main/kotlin/team/cklob/mudda/domain/member/application/ProfileAccessPolicy.kt
+++ src/main/kotlin/team/cklob/mudda/domain/member/application/ProfileAccessPolicy.kt
@@ -4,10 +4,12 @@ import team.cklob.mudda.domain.friend.domain.type.FriendStatus
 import team.cklob.mudda.domain.member.domain.type.ProfileVisibility
 
 object ProfileAccessPolicy {
-	fun canView(visibility: ProfileVisibility, isSelf: Boolean, friendStatus: FriendStatus): Boolean = when {
-		isSelf -> true
-		visibility == ProfileVisibility.PUBLIC -> true
-		visibility == ProfileVisibility.FRIEND -> friendStatus == FriendStatus.FRIEND
-		else -> false
+	fun canView(visibility: ProfileVisibility, isSelf: Boolean, friendStatus: FriendStatus): Boolean {
+		if (isSelf) return true
+		return when (visibility) {
+			ProfileVisibility.PUBLIC -> true
+			ProfileVisibility.FRIEND -> friendStatus == FriendStatus.FRIEND
+			ProfileVisibility.PRIVATE -> false
+		}
 	}
 }
